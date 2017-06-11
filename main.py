213fa2276439c6f7f466d17dc49d7e6e2c837238
Calvin4016 <hguo4@uw.edu>
2017-06-11 11:00:47 -0700

    Create main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ the pipeline and find line functions provided.
 '''
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
-Import image_processing_functions
+Import image_processing_functions as func
 
 # Set up lines for left and right
-left_lane = Line()
-right_lane = Line()
+left_lane = func.Line()
+right_lane = func.Line()
 white_output = 'lane_finding.mp4'
 clip1 = VideoFileClip("YOUR_VIDEO_FILE_GOES_HERE.mp4")
-white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip = clip1.fl_image(func.process_image) #NOTE: this function expects color images!!
 %time white_clip.write_videofile(white_output, audio=False)
